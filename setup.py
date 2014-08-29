07aa7967d8acb391532b27e0a238b75e0c1f618c
jkokorian <jkokorian@gmail.com>
2014-08-29 14:18:41 +0200

    nbopen => nbopen.py
    just on windows for the moment

--- setup.py
+++ setup.py
@@ -13,11 +13,7 @@ setup(name='nbopen',
       author_email="thomas@kluyver.me.uk",
       url="https://github.com/takluyver/nbopen",
       py_modules=['nbopen'],
-      entry_points={
-        'console_scripts': [
-          'nbopen=nbopen:main',
-        ],
-      },
+      scripts=['nbopen.py'],
       classifiers=[
           "Framework :: IPython",
           "License :: OSI Approved :: BSD License",
