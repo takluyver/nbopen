cf0703e0d2a31dee4af86a53add47a5ee6ed5f2a
Thomas Kluyver <takowl@gmail.com>
2014-09-25 17:43:35 -0700

    Add Windows installation
    GUI integration only, no command line nbopen at the moment.

--- setup.py
+++ setup.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
-
+import sys
 from distutils.core import setup
 
-with open('README.rst') as f:
-    readme = f.read()
+def main(argv=None):
+    if argv is not None:
+        # sarcastic message about distutils here
+        saved_argv = sys.argv[:]
+        sys.argv[1:] = argv
 
-setup(name='nbopen',
-      version='0.2',
-      description="Open a notebook from the command line in the best available server",
-      long_description=readme,
-      author='Thomas Kluyver',
-      author_email="thomas@kluyver.me.uk",
-      url="https://github.com/takluyver/nbopen",
-      py_modules=['nbopen'],
-      scripts=['nbopen'],
-      classifiers=[
-          "Framework :: IPython",
-          "License :: OSI Approved :: BSD License",
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 3",
-         ],
-     )
+    try:
+        with open('README.rst') as f:
+            readme = f.read()
+
+        setup(name='nbopen',
+              version='0.2',
+              description="Open a notebook from the command line in the best available server",
+              long_description=readme,
+              author='Thomas Kluyver',
+              author_email="thomas@kluyver.me.uk",
+              url="https://github.com/takluyver/nbopen",
+              py_modules=['nbopen'],
+              scripts=['nbopen'],
+              classifiers=[
+                  "Framework :: IPython",
+                  "License :: OSI Approved :: BSD License",
+                  "Programming Language :: Python :: 2",
+                  "Programming Language :: Python :: 3",
+                 ],
+             )
+
+    finally:
+        if argv is not None:
+            sys.argv = saved_argv
+
+if __name__ == '__main__':
+    main()
---
+++ win-install.py
@@ -0,0 +1,31 @@
+import sys
+import setup
+
+setup.main(['install', '--user'])
+
+import winreg
+
+SZ = winreg.REG_SZ
+with winreg.CreateKey(winreg.HKEY_CURRENT_USER, "Software\Classes\.ipynb") as k:
+    winreg.SetValue(k, "", SZ, "Jupyter.nbopen")
+    winreg.SetValueEx(k, "Content Type", 0, SZ, "application/x-ipynb+json")
+    winreg.SetValueEx(k, "PerceivedType", 0, SZ, "document")
+    with winreg.CreateKey(k, "OpenWithProgIds") as openwith:
+        winreg.SetValueEx(openwith, "Jupyter.nbopen", 0, winreg.REG_NONE, b'')
+
+executable = sys.executable
+if executable.endswith("python.exe"):
+    executable = executable[:-10] + 'pythonw.exe'
+launch_cmd = '"{}" -m nbopen "%1"'.format(executable)
+
+with winreg.CreateKey(winreg.HKEY_CURRENT_USER, "Software\Classes\Jupyter.nbopen") as k:
+    winreg.SetValue(k, "", SZ, "IPython notebook")
+    with winreg.CreateKey(k, "shell\open\command") as launchk:
+        winreg.SetValue(launchk, "", SZ, launch_cmd)
+
+try:
+    from win32com.shell import shell, shellcon
+    shell.SHChangeNotify(shellcon.SHCNE_ASSOCCHANGED, shellcon.SHCNF_IDLIST, None, None)
+except ImportError:
+    print("You may need to restart for association with .ipynb files to work")
+    print("  (pywin32 is needed to notify Windows of the change)")
